f98162539fa6654a241f64128b663f950f52dc5c
Bombarding <skruffy@aim.com>
2016-10-06 21:20:34 -0400

    Rainbow Pacman

---
+++ src/Pacman.java
@@ -0,0 +1,37 @@
+//Author: Alex Steel
+//Rainbow Pacman
+//Warning!! ***
+//****MAY CAUSE SEIZURES****
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+public class Pacman extends JFrame {
+
+    public Pacman() {
+        
+        initUI();
+    }
+    
+    private void initUI() {
+        
+        add(new Board());       //creates new board object     
+        setTitle("Pacman");     //Tiles window "Pacman"
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(380, 420);      //sets size of window
+        setLocationRelativeTo(null);    
+        setVisible(true);        
+    }
+
+    public static void main(String[] args) {
+
+        EventQueue.invokeLater(new Runnable() {
+
+            @Override
+            public void run() {
+                Pacman ex = new Pacman();
+                ex.setVisible(true);
+            }
+        });
+    }
+}
